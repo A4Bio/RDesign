a7e0f9a227322bead48ce94100a1e976692871ff
Yijie Zhang <44703867+BeforeSunrise710@users.noreply.github.com>
2024-08-16 22:21:19 -0400

    Update aug_dataset.py

--- API/aug_dataset.py
+++ API/aug_dataset.py
@@ -8,9 +8,10 @@ from .utils import cached_property
 
 
 class AugDataset(data.Dataset):
-    def __init__(self, path='./',  mode='train'):
+    def __init__(self, path='./',  mode='train', load_full_data= True):
         self.path = path
         self.mode = mode
+        self.load_full_data = load_full_data
         self.data = self.cache_data[mode]
     
     @cached_property
@@ -19,7 +20,11 @@ class AugDataset(data.Dataset):
         if os.path.exists(self.path):
             data_dict = {'train': [], 'val': [], 'test': []}
             # val and test data
-            for split in ['train', 'val', 'test']:
+            if self.load_full_data:
+                data_list = ['train', 'val', 'test']
+            else:
+                data_list = ['test']
+            for split in data_list:
                 data = cPickle.load(open(os.path.join(self.path, split + '_data.pt'), 'rb'))
                 for entry in tqdm(data):
                     for key, val in entry['coords'].items():
